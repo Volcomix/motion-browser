4f9203e960a25d505e31782a5b0024cfb48e776e
Volcomix <volcomix@gmail.com>
2018-03-10 19:44:23 +0100

    Init diff processing

--- Processor.js
+++ Processor.js
@@ -4,6 +4,10 @@ function processor(video) {
   class Processor {
     constructor(video) {
       this.video = video
+
+      this.width = 64
+      this.height = 48
+      this.pixelThreshold = 32
     }
 
     get isPlaying() {
@@ -44,8 +48,6 @@ function processor(video) {
     }
 
     startTimer() {
-      this.width = 64
-      this.height = 48
       this.canvas.width = this.width
       this.canvas.height = this.height
       this.diffCanvas.width = this.width
@@ -67,10 +69,65 @@ function processor(video) {
       this.diffContext.drawImage(this.video, 0, 0, this.width, this.height)
 
       const frame = this.diffContext.getImageData(0, 0, this.width, this.height)
+      const box = this.processDiff(frame)
       this.context.putImageData(frame, 0, 0)
+      this.drawBox(box)
 
       this.diffContext.globalCompositeOperation = 'source-over'
       this.diffContext.drawImage(this.video, 0, 0, this.width, this.height)
+    }
+
+    processDiff(frame) {
+      const rgba = frame.data
+      let score = 0
+      const box = this.initBox
+      for (let i = 0; i < rgba.length; i += 4) {
+        const diff = this.processPixelDiff(rgba, i)
+        if (diff >= this.pixelThreshold) {
+          score++
+          this.updateBox(box, i / 4)
+        }
+      }
+      return box.xMax > -1 ? box : undefined
+    }
+
+    processPixelDiff(rgba, i) {
+      const diff = rgba[i] * 0.3 + rgba[i + 1] * 0.6 + rgba[i + 2] * 0.1
+      rgba[i] = 0
+      rgba[i + 1] = Math.min(255, diff * 255 / this.pixelThreshold)
+      rgba[i + 2] = 0
+      return diff
+    }
+
+    get initBox() {
+      return {
+        xMin: this.width + 1,
+        xMax: -1,
+        yMin: this.height + 1,
+        yMax: -1,
+      }
+    }
+
+    updateBox(box, pixelIndex) {
+      const x = pixelIndex % this.width
+      const y = Math.floor(pixelIndex / this.width)
+      box.xMin = Math.min(box.xMin, x)
+      box.xMax = Math.max(box.xMax, x)
+      box.yMin = Math.min(box.yMin, y)
+      box.yMax = Math.max(box.yMax, y)
+    }
+
+    drawBox(box) {
+      if (!box) {
+        return
+      }
+      this.context.strokeStyle = 'white'
+      this.context.strokeRect(
+        box.xMin + 0.5,
+        box.yMin + 0.5,
+        box.xMax - box.xMin,
+        box.yMax - box.yMin,
+      )
     }
   }
 
